ee9e3e418fcf11d5ae9eb649deae9c75b8f4a97d
Nati Lavega <nati.lavega@gmail.com>
2022-05-19 13:13:13 -0300

    project setup

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+// init Firebase
+const firebase = initializeApp(firebaseConfig);
+
+// init services
+export const auth = getAuth(firebase);
+export const db = getFirestore(firebase);
